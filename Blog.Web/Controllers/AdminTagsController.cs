f5ef274b604d0c656a0cce722b2aa36adfd55fdc
carding1968 <113331846+carding1968@users.noreply.github.com>
2025-12-01 15:25:24 +0800

    first commit

---
+++ Blog.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,44 @@
+﻿using Blog.Web.Data;
+using Blog.Web.Models.Domain;
+using Blog.Web.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        private readonly BlogDbContext _blogDbContext;
+        public AdminTagsController(BlogDbContext blogDbContext)
+        {
+            _blogDbContext = blogDbContext;            
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddTagRequest addTagRequest) 
+        {
+            Tag tag = new Tag();
+            tag.Name = addTagRequest.Name;
+            tag.DisplayName = addTagRequest.DisplayName;
+            
+            _blogDbContext.Tags.Add(tag);
+            _blogDbContext.SaveChanges();
+            return View("Add");
+        }
+
+        [HttpGet]
+        public IActionResult List() {
+            var tags = _blogDbContext.Tags.ToList();
+
+
+
+            return View(tags);
+        }
+
+    }
+}
