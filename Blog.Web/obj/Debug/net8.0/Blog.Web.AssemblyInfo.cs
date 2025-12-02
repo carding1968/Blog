ba7bc19ac72b3abc5889831e229a115e873d3e4d
carding1968 <113331846+carding1968@users.noreply.github.com>
2025-12-02 15:03:04 +0800

    test

--- Blog.Web/obj/Debug/net8.0/Blog.Web.AssemblyInfo.cs
+++ Blog.Web/obj/Debug/net8.0/Blog.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5ef274b604d0c656a0cce722b2aa36adfd55fdc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a64484affea535188f0926ee282b5ad466440e92")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
